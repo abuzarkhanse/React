099d1490b29fb206c7f90799f6d4c92b01e3f6a6
Abuzar Khan <mrabuzar459@gmail.com>
2025-02-10 03:54:20 +0500

    Video 03 completed

---
+++ 01basicreact/src/abuzar.js
@@ -0,0 +1,8 @@
+function Abuzar() {
+    return(
+        <h3>Ready to Learn this is new file i created! in react-app</h3>
+    )
+    
+}
+
+export default Abuzar
--- 01basicreact/src/index.js
+++ 01basicreact/src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; // Implentation on web-page
+
 import App from './App';
+import Abuzar from './abuzar';
+
+//DOM is tree structure like in DSA
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// render mean displaying UI components on the screen
+// Like HTML output on Browser
+
 root.render(
+
+  // React strickMood is react property, it also work without this mood
+
   <React.StrictMode>
     <App />
+    <Abuzar />
   </React.StrictMode>
 );
