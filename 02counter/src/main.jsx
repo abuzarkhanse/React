c8bd879eb11e1e0559ef4ff50307cb085aa03b5c
Abuzar Khan <mrabuzar459@gmail.com>
2025-02-12 00:55:46 +0500

    Video 06 => Documentation About React...

--- 02counter/src/main.jsx
+++ 02counter/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Notes about createRoot please check Documentation Foldder video06
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
